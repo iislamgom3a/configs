fe24803e9fc40fdcfab406ebaf7b5b728df2ff98
Islam Gomaa <147432821+iislamgom3a@users.noreply.github.com>
2025-02-12 22:21:40 +0200

    Update download_dir_organizer.bat

--- windows_11/download_dir_organizer.bat
+++ windows_11/download_dir_organizer.bat
@@ -2,7 +2,7 @@
 setlocal
 
 :: Define the source directory (adjust to your actual Downloads directory)
-set "source_dir=%win11%\Downloads"
+set "source_dir=%C:\Users\win11%\Downloads"
 
 :: Create a log file to track the process
 set "log_file=%source_dir%\organization_log.log"
@@ -12,7 +12,7 @@ echo Organization started at %date% %time% >> "%log_file%"
 mkdir "%source_dir%\Images" "%source_dir%\Videos" "%source_dir%\Software" "%source_dir%\Archives" "%source_dir%\Docs" 
 
 :: Function to move files based on extension
-call :move_files "Images" jpg jpeg png gif bmp tiff webp heic raw svg
+call :move_files "Images" jpg jpeg png gif bmp tiff webp heic raw svg JPG
 call :move_files "Videos" mp4 mkv mov avi flv webm mpeg 3gp ts
 call :move_files "Software" exe
 call :move_files "Docs" doc docx dot dotx xls xlsx xlsm xlt xltx ppt pptx pps ppsx pot potx mdb accdb pub one odt ods odp odg odf pdf xps txt rtf csv log md xml html htm json tex epub chm djvu
