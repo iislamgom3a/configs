1e82ddc3f8593925735ba5f4b2542f35c895d4b0
Islam Gomaa <147432821+iislamgom3a@users.noreply.github.com>
2025-02-12 22:15:27 +0200

    Update download_dir_organizer (add more extentions)

--- windows_11/download_dir_organizer.bat
+++ windows_11/download_dir_organizer.bat
@@ -9,13 +9,14 @@ set "log_file=%source_dir%\organization_log.log"
 echo Organization started at %date% %time% >> "%log_file%"
 
 :: Create target directories based on file type
-mkdir "%source_dir%\Images" "%source_dir%\Videos" "%source_dir%\Software" "%source_dir%\Archives" "%source_dir%\Docs"
+mkdir "%source_dir%\Images" "%source_dir%\Videos" "%source_dir%\Software" "%source_dir%\Archives" "%source_dir%\Docs" 
 
 :: Function to move files based on extension
 call :move_files "Images" jpg jpeg png gif bmp tiff webp heic raw svg
 call :move_files "Videos" mp4 mkv mov avi flv webm mpeg 3gp ts
-call :move_files "Software" deb rpm xz tar.gz zip 7z
-call :move_files "Docs" pdf
+call :move_files "Software" exe
+call :move_files "Docs" doc docx dot dotx xls xlsx xlsm xlt xltx ppt pptx pps ppsx pot potx mdb accdb pub one odt ods odp odg odf pdf xps txt rtf csv log md xml html htm json tex epub chm djvu
+call :move_files "Archives" zip rar 7z tar gz bz2 xz iso cab arj lzh ace z arc jar war apk deb rpm pkg tar.gz tar.bz2 tar.xz tgz tbz txz
 
 :: Clean up empty directories
 for /d %%d in (%source_dir%\*) do rd "%%d" 2>nul
