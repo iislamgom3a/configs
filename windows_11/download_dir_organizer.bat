ea0059e1355e251580d2bbb196e20cd261198f46
Islam Gomaa <147432821+iislamgom3a@users.noreply.github.com>
2025-02-12 22:32:54 +0200

    Update download_dir_organizer.bat

--- windows_11/download_dir_organizer.bat
+++ windows_11/download_dir_organizer.bat
@@ -4,10 +4,6 @@ setlocal
 :: Define the source directory (adjust to your actual Downloads directory)
 set "source_dir=%C:\Users\win11%\Downloads"
 
-:: Create a log file to track the process
-set "log_file=%source_dir%\organization_log.log"
-echo Organization started at %date% %time% >> "%log_file%"
-
 :: Create target directories based on file type
 mkdir "%source_dir%\Images" "%source_dir%\Videos" "%source_dir%\Software" "%source_dir%\Archives" "%source_dir%\Docs" 
 
@@ -21,20 +17,13 @@ call :move_files "Archives" zip rar 7z tar gz bz2 xz iso cab arj lzh ace z arc j
 :: Clean up empty directories
 for /d %%d in (%source_dir%\*) do rd "%%d" 2>nul
 
-:: Provide a summary of the organization process
-echo Files have been organized successfully!
-echo Summary: >> "%log_file%"
+
 for /d %%d in (%source_dir%\*) do (
     if exist "%%d" (
         for /f "tokens=1,2" %%a in ('dir /s /a "%%d" ^| findstr /r "^ *[0-9]"') do (
-            echo %%b >> "%log_file%"
         )
     )
 )
-
-echo Organization completed at %date% %time% >> "%log_file%"
-echo ---------------------------------------------------------------- >> "%log_file%"
-echo ---------------------------------------------------------------- >> "%log_file%"
 
 exit /b
 
@@ -44,7 +33,6 @@ shift
 for %%e in (%*) do (
     for %%f in ("%source_dir%\*.%%e") do (
         move "%%f" "%source_dir%\%folder%\" >nul
-        echo Moved %%e files to %folder% >> "%log_file%"
     )
 )
 exit /b
