83771ac5833ecc308b63e1ce808c42002eb4e591
iislamgom3a <iislamgom3a@gmail.com>
2025-03-08 04:35:43 +0200

    1-delete unneeded configs. 2-update organizer script

--- windows_11/download_dir_organizer.bat
+++ windows_11/download_dir_organizer.bat
@@ -2,18 +2,19 @@
 setlocal
 
 :: Define the source directory (adjust to your actual Downloads directory)
-set "source_dir=%C:\Users\win11%\Downloads"
+set "source_dir=%C:\Users\iislamgom3a%\Downloads"
 
 :: Create target directories based on file type
-mkdir "%source_dir%\Images" "%source_dir%\Videos" "%source_dir%\Software" "%source_dir%\Archives" "%source_dir%\Docs" 
+mkdir "%source_dir%\Images" "%source_dir%\Videos" "%source_dir%\Software" "%source_dir%\Archives" "%source_dir%\Docs" "%source_dir%\Audio" "%source_dir%\src"
 
 :: Function to move files based on extension
 call :move_files "Images" jpg jpeg png gif bmp tiff webp heic raw svg JPG
-call :move_files "Videos" mp4 mkv mov avi flv webm mpeg 3gp ts
+call :move_files "Videos" mp4 mkv mov avi flv webm mpeg 3gp ts wav
 call :move_files "Software" exe
 call :move_files "Docs" doc docx dot dotx xls xlsx xlsm xlt xltx ppt pptx pps ppsx pot potx mdb accdb pub one odt ods odp odg odf pdf xps txt rtf csv log md xml html htm json tex epub chm djvu
 call :move_files "Archives" zip rar 7z tar gz bz2 xz iso cab arj lzh ace z arc jar war apk deb rpm pkg tar.gz tar.bz2 tar.xz tgz tbz txz
-
+call :move_files "Audio" mp3 m4a
+call :move_files "src" py cpp c java ipynp 
 :: Clean up empty directories
 for /d %%d in (%source_dir%\*) do rd "%%d" 2>nul
 
